11d52ed8c3d863598b1d6026207d656841b99117
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-06-11 22:24:28 -0500

    Random Password Generator code

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -4,6 +4,15 @@ Created on Sat Jun  6 22:47:23 2020
 
 @author: Emmanuel
 """
-from platform import python_version
+import random 
 
-print(python_version())
+chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#~€¬/"
+number = int(input('Numbers of password?: '))
+
+lenght = int(input('Password lenght?: '))
+
+for p in range(number):
+    password = ''
+    for c in range(lenght):
+        password += random.choice(chars)
+    print(password)
