8fb04468996f4a804deaab9a2f1a13594e2063db
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-06-12 00:06:03 -0500

    I start the GUI and I setup root with a icon

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -5,6 +5,12 @@ Created on Sat Jun  6 22:47:23 2020
 @author: Emmanuel
 """
 import random 
+from tkinter import *
+
+# Setup root
+root = Tk()
+root.title("Random Password Generator")
+root.iconbitmap('ink-bottle-and-a-feather-to-write_icon-icons.com_56812.ico')
 
 # Characters that can be used in the password
 chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#~€¬/"
@@ -23,3 +29,5 @@ for p in range(number):
     # To show password
     print(password)
     
+# Main loop 
+root.mainloop()
