6bc69f52f25e1d777f840de94d52abc435369b7f
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-06-11 23:22:11 -0500

    To add comments

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -6,13 +6,20 @@ Created on Sat Jun  6 22:47:23 2020
 """
 import random 
 
+# Characters that can be used in the password
 chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#~€¬/"
+# Numbers of password to generate
 number = int(input('Numbers of password?: '))
 
+# Password lenght 
 lenght = int(input('Password lenght?: '))
 
+# This for generate a first password
 for p in range(number):
     password = ''
+    # for nested that generate a password  
     for c in range(lenght):
         password += random.choice(chars)
+    # To show password
     print(password)
+    
