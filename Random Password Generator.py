0c94459e25764c15506ee893923590ce3b3e46bd
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-06-23 00:11:35 -0500

    I finished the functionality of the program

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -7,10 +7,17 @@ Created on Sat Jun  6 22:47:23 2020
 import random 
 from tkinter import *
 
-
 # Characters that can be used in the password
 chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#~€¬/"
 
+class WritablesStringVar(StringVar):
+    
+    def write(self, added_text):
+        new_text = self.get() + added_text
+        self.set(new_text)
+        
+    def clear(self):
+        self.set("")
 
 def password_generator():
     global chars
@@ -21,6 +28,9 @@ def password_generator():
     # Password lenght 
     password_length.get()
     
+    # To avoid the last pack of password
+    final_password.clear()
+    
     password = ''
     
     try:
@@ -30,9 +40,10 @@ def password_generator():
             # for nested that generate a password  
             for c in range(int(password_length.get())):
                 password += random.choice(chars)
-            password += '\n\n'
-        # To set password
-        final_password.set(password)
+            print(password, file = final_password)
+            # To avoid the characters of the last iteration
+            password = ''
+        
         
     except ValueError:
        messages =  Label(root, text = "Error!: please type a integer number")
@@ -47,25 +58,37 @@ root.iconbitmap('ink-bottle-and-a-feather-to-write_icon-icons.com_56812.ico')
 # Variables
 password_number = StringVar()
 password_length = StringVar()
-final_password = StringVar()
+final_password = WritablesStringVar(root)
 
 # Label password number
 pass_num = Label(root, text = "Numbers of Password")
 pass_num.pack()
 pass_num.config(font = ('times new roman', 12))
-Entry(root, justify="center", textvariable = password_number).pack()
+
+# Entry password number
+entry_pass_num = Entry(root, justify="center", textvariable = password_number)
+entry_pass_num.pack()
+
 
 # Label password lenght
 pass_len = Label(root, text = "Password Lenght")
 pass_len.pack()
 pass_len.config(font = ('times new roman', 12))
-Entry(root, justify="center", textvariable = password_length).pack()
+
+# Entry password number
+entry_pass_len = Entry(root, justify="center", textvariable = password_length)
+entry_pass_len.pack()
+
 
 # Label final password
 final_pass = Label(root, text = "Password")
 final_pass.pack()
 final_pass.config(font = ('times new roman', 12))
-Entry(root, justify="center", text = final_password, state = "disabled").pack()
+
+# Label to show final password
+show_passwords = Label(root, justify="center", textvariable = final_password, relief = GROOVE)
+show_passwords.pack()
+
 
 # Create button
 gen_pass = Button(root, text = "Generate Password", justify = "center", command = password_generator)
